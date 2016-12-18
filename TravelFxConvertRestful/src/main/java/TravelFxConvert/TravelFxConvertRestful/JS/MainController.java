4824e926f7c803351e441574fa40db6eb7a2af34
dexterchan <dexterchan@gmail.com>
2016-12-18 20:06:30 +0800

    checkin web

--- TravelFxConvertRestful/src/main/java/TravelFxConvert/TravelFxConvertRestful/JS/MainController.java
+++ TravelFxConvertRestful/src/main/java/TravelFxConvert/TravelFxConvertRestful/JS/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class MainController {
 
-    @RequestMapping(value="/angularjs",method = RequestMethod.GET)
+    @RequestMapping(value="/",method = RequestMethod.GET)
     public String homepage(){
         return "indexangularjs.html";
     }
